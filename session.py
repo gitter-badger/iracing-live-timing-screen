5e0b3c6eccc9ccec83c7f89894dd463162313fd2
Adrian Garcia <adrian.334458@gmail.com>
2015-08-06 20:58:10 -0400

    Create fix_driver_laptime(), adds missing digit from irsdk lap times

--- session.py
+++ session.py
@@ -1,6 +1,7 @@
 #
 class Session:
     """
+
     Session object
     """
 
@@ -10,6 +11,7 @@ class Session:
 
     def session_state_qualify(self):
         """
+
         :return: True if  iRacing session state is Qualify or Lone Qualify
         """
         if "Qualify" in self.ir['SessionInfo']['Sessions'][self.ir['SessionNum']]['SessionType']:
@@ -20,11 +22,12 @@ class Session:
     def update_classification(self):
         for idx, driver in enumerate(self.ir['SessionInfo']['Sessions'][0]['ResultsPositions']):
             m, s = divmod(driver['FastestTime'], 60)
+            s = self.fix_driver_laptime(str(s))
 
             self.classification['data'].append({
                 'CarIdx': driver['CarIdx'],
                 'Position': driver['Position'],
-                'FastestTime': '{}:{}'.format(int(m), str(s)[:6]),
+                'FastestTime': '{}:{}'.format(int(m), s),
                 'UserName': self.get_driver_name(driver['CarIdx'])
             })
 
@@ -33,3 +36,17 @@ class Session:
         for driver in self.ir['DriverInfo']['Drivers']:
             if driver['CarIdx'] == idx:
                 return driver['UserName']
+
+    def fix_driver_laptime(self, second):
+        """
+        :param second: str
+        :return: str
+
+        irsdk sometimes gives laptime data like this: '106.34'
+        here I just add a zero because I think iracing omits it.
+        # TODO test this.
+        """
+        if len(second) > 5:
+            return second[:6]
+        else:
+            return second[:5] + '0'
